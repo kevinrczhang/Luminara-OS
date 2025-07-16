6299d0f49ca227ade8f82f5be8db0b587dc79e2c
Jacob Wu <99063771+wu-jacob@users.noreply.github.com>
2025-07-15 20:02:50 -0700

    Add PCI header

---
+++ include/pci.h
@@ -0,0 +1,42 @@
+#ifndef PCI_H
+#define PCI_H
+
+#include "port.h"
+
+class PeripheralComponentInterconnectDeviceDescriptor
+{
+    public:
+        uint16_t bus_number;
+        uint16_t device_number;
+        uint16_t function_number;
+
+        uint16_t vendor_id;
+        uint16_t device_id;
+
+        uint8_t class_code;
+        uint8_t subclass;
+        uint8_t programming_interface;
+
+        uint8_t revision_id;
+
+        PeripheralComponentInterconnectDeviceDescriptor();
+        ~PeripheralComponentInterconnectDeviceDescriptor();
+};
+
+class PeripheralComponentInterconnectController
+{
+    Port32Bit command_port;
+    Port32Bit data_port;
+    
+    public:
+        PeripheralComponentInterconnectController();
+        ~PeripheralComponentInterconnectController();
+        
+        uint32_t read(uint16_t bus_number, uint16_t device_number, uint16_t function_number, uint32_t register_offset);
+        void write(uint16_t bus_number, uint16_t device_number, uint16_t function_number, uint32_t register_offset, uint32_t value);
+        bool device_has_functions(uint16_t bus_number, uint16_t device_number);
+        
+        PeripheralComponentInterconnectDeviceDescriptor get_device_descriptor(uint16_t bus_number, uint16_t device_number, uint16_t function_number);
+};
+
+#endif
